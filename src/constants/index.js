a4ca745011b4d99984ac19151022f17875ae213b
mandrusanjay123 <mandrusanjaykumar@gmail.com>
2026-04-11 16:39:34 -0700

    updated address

--- src/constants/index.js
+++ src/constants/index.js
@@ -173,7 +173,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "1525 Amherst Manor Dr Apt 802, Buffalo, NY",
+  address: "15606 NE 40th st Redmond, WA",
   // phoneNo: "+1 (716) 709-0989",
   email: "mandrusanjaykumar@gmail.com"
 };
